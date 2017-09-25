fd8540bbaf093026a4decc44a8f3dcb7669a477f
hyi <hyi@hbk.com>
2017-09-25 13:10:56 -0500

    up

--- R/random_forest.R
+++ R/random_forest.R
@@ -1,5 +1,5 @@
-# regression decicion tree is unstable. Random forest method will make the
-# result more stable.
+# decicion tree is unstable. Random forest method, create a set of decicion
+# trees and then add up the result, will make the result more stable.
 
 require(randomForest)
 require(MASS)  # package which contains the boston housing dataset
@@ -22,7 +22,7 @@ for (mtry in 1:13) {
     rf = randomForest(medv ~ ., data=Boston, subset=train, mtry=mtry, ntree=400)
     oob.err[mtry] = rf$mse[400]
     pred <- predict(rf, Boston[-train,])
-    browser()
+    #browser()
     test.err[mtry] = with(Boston[-train,], mean((medv-pred)^2))
     cat(mtry, "")
 }
