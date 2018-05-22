a42693dd5d1fd8ddaee7c95ceb10777e0eac116e
hyi <hyi@hbk.com>
2018-05-22 14:13:43 -0500

    up

--- mtrack/client/mtrackApp/src/app/certificate/certificate.component.ts
+++ mtrack/client/mtrackApp/src/app/certificate/certificate.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { XsignService } from '../xsign.service';
 
 @Component({
   selector: 'app-certificate',
@@ -7,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CertificateComponent implements OnInit {
 
-  constructor() { }
+  title = "Add Certificate";
+  angForm: FormGroup;
+  result: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private fb: FormBuilder, private xsignService: XsignService) {
+    this.createForm();
+   }
+
+  // TODO: make it a dynamic form
+  createForm() {
+    this.angForm = this.fb.group({
+      company: ['', Validators.required ],
+      pubkey: ['', Validators.required ],
+   });
+  }
+
+  add(company, pubkey) {
+    console.log(company, pubkey); // The args are passed by temp form elt ref
+    console.log(this.angForm.value); // The args are passed as json obj
+  }
 
   ngOnInit() {
   }
