dac7e150f03cf75d64269cf55dfb0ef76b10cf6e
Haoran Yi <haoran.yi@gmail.com>
2019-05-04 10:05:29 -0500

    up

--- cpp/test_variant.cpp
+++ cpp/test_variant.cpp
@@ -39,9 +39,61 @@ int main()
     y = "xyz"s;
     assert(std::holds_alternative<std::string>(y));
 
-    // TODO std::visit
-    // functor: https://www.bfilipek.com/2018/09/visit-variants.html
-    // lambda: https://en.cppreference.com/w/cpp/utility/variant/visit
+}
 
+// std::visit
+// functor: https://www.bfilipek.com/2018/09/visit-variants.html
+// lambda: https://en.cppreference.com/w/cpp/utility/variant/visit
+//   https://www.bfilipek.com/2018/06/variant.html#overload
+//   create an overload struct, that inherits from all lambda operator()
+struct Fluid { };
+struct LightItem { };
+struct HeavyItem { };
+struct FragileItem { };
 
+struct VisitPackage
+{
+    void operator()(Fluid& ) { cout << "fluid\n"; }
+    void operator()(LightItem& ) { cout << "light item\n"; }
+    void operator()(HeavyItem& ) { cout << "heavy item\n"; }
+    void operator()(FragileItem& ) { cout << "fragile\n"; }
+};
+
+void test_variant_visit_functor()
+{
+    std::variant<Fluid, LightItem, HeavyItem, FragileItem> package { 
+        FragileItem() };
+    std::visit(VisitPackage(), package);
+}
+
+// helper struct overload that inherits from all lambda's operator()
+template<class... Ts> struct overload : Ts... { using Ts::operator()...; };
+template<class... Ts> overload(Ts...) -> overload<Ts...>;
+
+void test_vairant_visit_lambda()
+{
+    std::variant<Fluid, LightItem, HeavyItem, FragileItem> package;
+
+    std::visit(overload{
+        [](Fluid& ) { cout << "fluid\n"; },
+        [](LightItem& ) { cout << "light item\n"; },
+        [](HeavyItem& ) { cout << "heavy item\n"; },
+        [](FragileItem& ) { cout << "fragile\n"; }
+    }, package);
+}
+
+// visit mutliple variant. must provide implementation for all type
+// combinations.
+void test_variant_visit_many()
+{
+
+    std::variant<LightItem, HeavyItem> basicPackA;
+    std::variant<LightItem, HeavyItem> basicPackB;
+
+    std::visit(overload{
+            [](LightItem&, LightItem& ) { cout << "2 light items\n"; },
+            [](LightItem&, HeavyItem& ) { cout << "light & heavy items\n"; },
+            [](HeavyItem&, LightItem& ) { cout << "heavy & light items\n"; },
+            [](HeavyItem&, HeavyItem& ) { cout << "2 heavy items\n"; },
+            }, basicPackA, basicPackB);
 }
