805bf607f046d91ec75af6c4b5cbac46fed241c5
hyi <hyi@hbk.com>
2019-05-10 09:47:37 -0500

    up

--- cpp/leetcode/solution.h
+++ cpp/leetcode/solution.h
@@ -3,15 +3,20 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        // HashMap
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        // use HashMap for O(1) lookup
         std::unordered_map<int, int> record;
-        for (int i = 0; (size_t)i != nums.size(); ++i) {
+        for (int i = 0; (size_t)i != nums.size(); ++i)
+        {
             auto found = record.find(nums[i]);
             if (found != record.end())
+            {
                 return {found->second, i};
+            }
             record.emplace(target - nums[i], i);
         }
         return {-1, -1};
