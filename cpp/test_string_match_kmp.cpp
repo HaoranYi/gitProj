8774b693a1ca544c40c7b6e6b6560a6f831f1ec4
hyi <hyi@hbk.com>
2019-05-23 15:29:56 -0500

    up

--- cpp/test_string_match_kmp.cpp
+++ cpp/test_string_match_kmp.cpp
@@ -2,4 +2,13 @@
 // naive algorithm has bad performance for repeated string
 // key insight: skip matched string
 // precompute overlap for match words
-// T() -- back track table -- compute recursively
+// T() -- back track table -- compute recursively -- O(m) -- linear to the
+// lenght of match words
+
+int main()
+{
+    //TOOD
+    if (true) {
+
+    }
+}
