2cbc0f2b5c35c618cee760772cc686ef1d004ee0
Haoran Yi <haoran.yi@gmail.com>
2022-02-16 17:39:23 -0600

    up

--- rust_playpen/src/my_class.rs
+++ rust_playpen/src/my_class.rs
@@ -1,4 +1,6 @@
-// pattern to define user class
+// pattern to define your own user class
+
+use std::ops::Range;
 
 #[derive(Debug, Clone)]  // macro for compiler code gen
 struct Session {         // define data fields in the class
@@ -7,16 +9,40 @@ struct Session {         // define data fields in the class
 }
 
 impl Session {
-    fn new(x: &str) -> Self {  // constructor
+    // constructor
+    fn new(x: &str) -> Self {
         Self {a: x.to_string(), b: x.to_string()}
     }
 
-    fn hello(&self) {  // method
-        println!("{} {}", self.a, self.b)
+    // another constructor returns Option<T>
+    fn new_from(x: i32) -> Option<Self> {
+        Some(Self {a: x.to_string(), b: (x+1).to_string()})
+    }
+
+    // method
+    fn hello(&self) {
+        println!("{} {}", self.a, self.b);
+    }
+
+    // another method
+    fn bar(&self) {
+        println!("another method bar");
+    }
+
+    // no infer for return type
+    fn gen_range(&self) -> Range<i32> {
+        1..10
     }
 }
 
 fn main() {
     let s = Session::new("abc");
     s.hello();
+    s.bar();
+
+    let s = Session::new_from(1).unwrap();
+    s.hello();
+    s.bar();
+
+    println!("{:?}", s.gen_range());
 }
