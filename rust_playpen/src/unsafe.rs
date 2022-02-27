8db896f928ee29317e81660898f80a9f1536e9a0
Haoran Yi <haoran.yi@gmail.com>
2022-02-27 10:09:20 -0600

    up

---
+++ rust_playpen/src/unsafe.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut a = 3;
+    // create raw pointer in safe code.
+    // The following code is not valid when using reference, two refs read and write can't be
+    // borrowed together.
+    let b = &mut a as *mut _;
+    let b2 = &a as *const _;
+    // raw pointer can only be deferenced in unsafe code
+    unsafe {
+      *b += 1;
+      println!("{}", *b2);
+    }
+    println!("{}", a);
+
+    // convert raw pointer to an Option<&T>, which can then be used in safe code
+    let c = unsafe {b.as_ref()};
+    println!("{:?}", c);
+}
