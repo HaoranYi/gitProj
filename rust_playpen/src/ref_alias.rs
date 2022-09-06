54ccd06f7d390c581d10a6cd1dbd46e167cd3196
Haoran Yi <haoran.yi@gmail.com>
2022-09-06 16:14:27 -0500

    PhantomData

--- rust_playpen/src/ref_alias.rs
+++ rust_playpen/src/ref_alias.rs
@@ -1,5 +1,20 @@
 // Rust Tip: use type alias with lifetime parameter
+use std::marker::PhantomData;
+
 type VecRef<'a, T> = &'a Vec<T>;
+
+// struct with lifetime parameter associated with the type parameter
+// this is how Iter<'a, T> are defined, which associate continer's lifetime with Iter through
+// v.iter() method:
+//     fn vec::iter(&self) -> Iter<'_, T>
+// and then pass the lifetime (from the container) down to member fn returns, i.e. ref to the
+// element
+//     fn Iter::next(&self) -> Option<&'a T>
+// To achieve this relay of lifetime parameter passing throught rust lifetime analyzer in the
+// compiler, use PhantomData marker to enforce the lifetime restriction.
+pub struct Foo<'a, T: 'a> {
+    _marker: PhantomData<&'a T>,
+}
 
 fn main() {
     //let v = vec![1, 2, 3];
@@ -9,4 +24,9 @@ fn main() {
 
     println!("{:?}", v);
     println!("{:?}", r);
+
+    // instantiation
+    let _f = Foo::<'static, i32> {
+        _marker: PhantomData,
+    };
 }
